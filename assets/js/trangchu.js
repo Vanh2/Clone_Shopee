cd5cb364147173f094224e290d9ef23ce9f947da
Vanh2 <0917799149a@gmail.com>
2021-12-23 16:36:47 +0700

    up lan 1

---
+++ assets/js/trangchu.js
@@ -0,0 +1,9 @@
+$(document).ready(function() {
+    $('.responsive').slick({
+        dots: true,
+        infinite: false,
+        speed: 300,
+        slidesToShow: 10,
+        slidesToScroll: 3,
+    });
+});
